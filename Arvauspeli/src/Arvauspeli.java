6505ea2a2fa218dddd07b36d6273593c2b420fb4
henaje <henri.uusitalo@student.hamk.fi>
2024-10-14 14:54:44 +0300

    Toinen versio

--- Arvauspeli/src/Arvauspeli.java
+++ Arvauspeli/src/Arvauspeli.java
@@ -8,21 +8,30 @@ public class Arvauspeli {
         // Käytetään Scanneria
         Scanner scanner = new Scanner(System.in);
         
-        // Toistetaan kysymystä, kunnes arvaus on oikea
+        int arvausKerrat = 0; // Muuttuja arvausten laskemiseen
+        
+        // Toistetaan kysymystä, kunnes arvaus on oikea tai käyttäjä kirjoittaa "loppu"
         while (true) {
-            System.out.print("Arvaa nimi: ");
+            System.out.print("Arvaa nimi (tai kirjoita 'loppu' lopettaaksesi): ");
             String arvaus = scanner.nextLine();
+            arvausKerrat++; // Lisätään arvausyritysten määrää
+            
+            // Tarkistetaan, haluaako käyttäjä lopettaa pelin
+            if (arvaus.equalsIgnoreCase("loppu")) {
+                System.out.println("Peli loppui. Arvauksia tehtiin yhteensä: " + arvausKerrat);
+                break;
+            }
             
             // Tarkistetaan, onko arvaus oikea
             if (arvaus.equalsIgnoreCase(oikeaNimi)) {
-                System.out.println("Arvasit oikein!");
+                System.out.println("Onneksi olkoon! Arvasit oikein " + arvausKerrat + ". yrittämällä!");
                 break; // Lopetetaan looppi, jos arvaus on oikea
             } else {
                 System.out.println("Väärin! Yritä uudestaan.");
             }
         }
         
-        // Suljetaan scanneri
+        // Suljetaan scanner
         scanner.close();
     }
 }
